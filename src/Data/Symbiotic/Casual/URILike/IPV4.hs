c17154fb716248329617219b2f2cda48a7c2c1ef
Athan Clark <athan.clark@gmail.com>
2019-10-25 15:32:08 -0700

    ip

---
+++ src/Data/Symbiotic/Casual/URILike/IPV4.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE
+    DeriveGeneric
+  #-}
+
+module Data.Symbiotic.Casual.URILike.IPV4 where
+
+import Net.IPv4 (IPv4, encode, decode, toOctets, fromOctets)
+import Data.Aeson (ToJSON (..), FromJSON (..), Value (String))
+import Data.Aeson.Types (typeMismatch)
+import Data.Serialize (Serialize (..))
+import Data.Serialize.Put (putWord8)
+import Data.Serialize.Get (getWord8)
+import GHC.Generics (Generic)
+
+
+newtype IPV4 = IPV4 {getIPV4 :: IPv4}
+  deriving (Generic, Eq, Ord, Show)
+
+instance ToJSON IPV4 where
+  toJSON = String . encode . getIPV4
+
+instance FromJSON IPV4 where
+  parseJSON json = case json of
+    String s -> case decode s of
+      Nothing -> fail'
+      Just x -> pure (IPV4 x)
+    _ -> fail'
+    where
+      fail' = typeMismatch "IPV4" json
+
+instance Serialize IPV4 where
+  put (IPV4 ip) =
+    let (a,b,c,d) = toOctets ip
+    in  putWord8 a *> putWord8 b *> putWord8 c *> putWord8 d
+  get = IPV4 <$> (fromOctets <$> getWord8 <*> getWord8 <*> getWord8 <*> getWord8)
---
+++ src/Data/Symbiotic/Casual/URILike/IPV6.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE
+    DeriveGeneric
+  #-}
+
+module Data.Symbiotic.Casual.URILike.IPV6 where
+
+import Net.IPv6 (IPv6, encode, decode, toWord16s, fromWord16s)
+import Data.Aeson (ToJSON (..), FromJSON (..), Value (String))
+import Data.Aeson.Types (typeMismatch)
+import Data.Serialize (Serialize (..))
+import Data.Serialize.Put (putWord16be)
+import Data.Serialize.Get (getWord16be)
+import GHC.Generics (Generic)
+
+
+newtype IPV6 = IPV6 {getIPV6 :: IPv6}
+  deriving (Generic, Eq, Ord, Show)
+
+instance ToJSON IPV6 where
+  toJSON = String . encode . getIPV6
+
+instance FromJSON IPV6 where
+  parseJSON json = case json of
+    String s -> case decode s of
+      Nothing -> fail'
+      Just x -> pure (IPV6 x)
+    _ -> fail'
+    where
+      fail' = typeMismatch "IPV6" json
+
+instance Serialize IPV6 where
+  put (IPV6 ip) =
+    let (a,b,c,d,e,f,g,h) = toWord16s ip
+    in  putWord16be a
+        *> putWord16be b
+        *> putWord16be c
+        *> putWord16be d
+        *> putWord16be e
+        *> putWord16be f
+        *> putWord16be g
+        *> putWord16be h
+  get = IPV6 <$>
+        ( fromWord16s
+          <$> getWord16be
+          <*> getWord16be
+          <*> getWord16be
+          <*> getWord16be
+          <*> getWord16be
+          <*> getWord16be
+          <*> getWord16be
+          <*> getWord16be
+        )
