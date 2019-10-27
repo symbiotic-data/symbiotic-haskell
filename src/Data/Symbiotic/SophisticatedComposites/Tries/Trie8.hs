6eeeb6b5d18a05ba48fbb610fbc5c656c1e3107b
Athan Clark <athan.clark@gmail.com>
2019-10-27 16:22:51 -0700

    moving orphan instances

--- src/Data/Symbiotic/SophisticatedComposites/Tries/Trie8.hs
+++ src/Data/Symbiotic/SophisticatedComposites/Tries/Trie8.hs
@@ -5,7 +5,7 @@
 
 module Data.Symbiotic.SophisticatedComposites.Tries.Trie8 where
 
-import Data.Symbiotic.SophisticatedComposites.Mappings.Map8 (Map8 (..))
+import Data.Symbiotic.SophisticatedComposites.Tries.Trie8.Orphans ()
 
 import qualified Data.Trie.Map as MT
 import qualified Data.Map as Map
@@ -45,25 +45,3 @@ instance (FromJSON k, FromJSON a, Ord k) => FromJSON (Trie8 k a) where
             pure (MT.MapChildren my ys)
 
       fail' = typeMismatch "Trie8" json
-
--- instance (Serialize k, Serialize a) => Serialize (Trie8 k a) where
---   put (Trie8 xs) =
-
-instance (Serialize a, Serialize (c p a)) => Serialize (MT.MapChildren c p a) where
-  put (MT.MapChildren my ys) = do
-    put my
-    put ys
-  get = do
-    my <- get
-    ys <- get
-    pure (MT.MapChildren my ys)
-
-instance (Serialize a, Serialize p, Ord p, Serialize (c p a)) => Serialize (MT.MapStep c p a) where
-  put (MT.MapStep xs) = put (Map8 xs)
-  get = do
-    Map8 xs <- get
-    pure (MT.MapStep xs)
-
-instance (Serialize a, Serialize k, Ord k) => Serialize (MT.MapTrie k a) where
-  put (MT.MapTrie xs) = put xs
-  get = MT.MapTrie <$> get
